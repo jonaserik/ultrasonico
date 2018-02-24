732896f12df140927b7e0dc3934c516c0cd98f44
Eduardo <eduardo@SlackSilver.local>
2018-02-23 21:11:39 -0300

    sensor ultrasonico enviando dados para fila MQTT

--- sensor_ultrasonico3/sensor_ultrasonico3.ino
+++ sensor_ultrasonico3/sensor_ultrasonico3.ino
@@ -5,26 +5,26 @@
 #include <Ultrasonic.h>           //Leitura de distância com o sensor HCSR04
 #include <PubSubClient.h>         // Importa a Biblioteca PubSubClient
 
-Ultrasonic ultrassom(4,5); // define o nome do sensor(ultrassom)
+Ultrasonic ultrassom(4, 5); // define o nome do sensor(ultrassom)
 //e onde esta ligado o trig(4) e o echo(5) respectivamente
 long distancia;
+//char* distancia;
 
- 
 //defines:
 //defines de id mqtt e tópicos para publicação e subscribe
 #define TOPICO_SUBSCRIBE "29asoMqttPresencaOutput"     //tópico MQTT de escuta
 #define TOPICO_PUBLISH   "29asoMqttPresencaInput"    //tópico MQTT de envio de informações para Broker
-                                                   //IMPORTANTE: recomendamos fortemente alterar os nomes
-                                                   //            desses tópicos. Caso contrário, há grandes
-                                                   //            chances de você controlar e monitorar o NodeMCU
-                                                   //            de outra pessoa.
+//IMPORTANTE: recomendamos fortemente alterar os nomes
+//            desses tópicos. Caso contrário, há grandes
+//            chances de você controlar e monitorar o NodeMCU
+//            de outra pessoa.
 #define ID_MQTT  "29asoMqttPres"  //id mqtt (para identificação de sessão)
-                                  //IMPORTANTE: este deve ser único no broker (ou seja, 
-                                  //            se um client MQTT tentar entrar com o mesmo 
-                                  //            id de outro já conectado ao broker, o broker 
-                                  //            irá fechar a conexão de um deles).
-                               
- 
+//IMPORTANTE: este deve ser único no broker (ou seja,
+//            se um client MQTT tentar entrar com o mesmo
+//            id de outro já conectado ao broker, o broker
+//            irá fechar a conexão de um deles).
+
+
 //defines - mapeamento de pinos do NodeMCU
 #define D0    16
 #define D1    5
@@ -38,224 +38,230 @@ long distancia;
 #define D9    3
 #define D10   1
 #define led   16
- 
- 
+
+
 // WIFI
 const char* SSID = "DarkStar"; // SSID / nome da rede WI-FI que deseja se conectar
 const char* PASSWORD = "iot123123"; // Senha da rede WI-FI que deseja se conectar
- 
+
 // MQTT
 const char* BROKER_MQTT = "iot.eclipse.org"; //URL do broker MQTT que se deseja utilizar
 int BROKER_PORT = 1883; // Porta do Broker MQTT
- 
- 
+
+
 //Variáveis e objetos globais
 WiFiClient espClient; // Cria o objeto espClient
 PubSubClient MQTT(espClient); // Instancia o Cliente MQTT passando o objeto espClient
 char EstadoSaida = '0';  //variável que armazena o estado atual da saída
- 
+
 //Prototypes
 void initSerial();
 void initWiFi();
 void initMQTT();
-void reconectWiFi(); 
+void reconectWiFi();
 void mqtt_callback(char* topic, byte* payload, unsigned int length);
 void VerificaConexoesWiFIEMQTT(void);
 void InitOutput(void);
- 
-/* 
- *  Implementações das funções
- */
-void setup() 
+
+/*
+    Implementações das funções
+*/
+void setup()
 {
-    //inicializações:
-    InitOutput();
-    initSerial();
-    initWiFi();
-    initMQTT();
-    Serial.begin(115200); //Habilita Comunicação Serial a uma taxa de 115200 bauds.
-    //WiFiManager wifiManager;
-    //wifiManager.autoConnect("ultra", "ultra");
-    //Serial.println("connected...yeey :)");
+  //inicializações:
+  InitOutput();
+  initSerial();
+  initWiFi();
+  initMQTT();
+  Serial.begin(115200); //Habilita Comunicação Serial a uma taxa de 115200 bauds.
+  //WiFiManager wifiManager;
+  //wifiManager.autoConnect("ultra", "ultra");
+  //Serial.println("connected...yeey :)");
 }
 
-//Função: inicializa comunicação serial com baudrate 115200 (para fins de monitorar no terminal serial 
+//Função: inicializa comunicação serial com baudrate 115200 (para fins de monitorar no terminal serial
 //        o que está acontecendo.
 //Parâmetros: nenhum
 //Retorno: nenhum
-void initSerial() 
+void initSerial()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
 }
- 
+
 //Função: inicializa e conecta-se na rede WI-FI desejada
 //Parâmetros: nenhum
 //Retorno: nenhum
-void initWiFi() 
+void initWiFi()
 {
-    delay(10);
-    Serial.println("------Conexao WI-FI------");
-    Serial.print("Conectando-se na rede: ");
-    Serial.println(SSID);
-    Serial.println("Aguarde");
-    
-    reconectWiFi();
+  delay(10);
+  Serial.println("------Conexao WI-FI------");
+  Serial.print("Conectando-se na rede: ");
+  Serial.println(SSID);
+  Serial.println("Aguarde");
+
+  reconectWiFi();
 }
- 
-//Função: inicializa parâmetros de conexão MQTT(endereço do 
+
+//Função: inicializa parâmetros de conexão MQTT(endereço do
 //        broker, porta e seta função de callback)
 //Parâmetros: nenhum
 //Retorno: nenhum
-void initMQTT() 
+void initMQTT()
 {
-    MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
-    MQTT.setCallback(mqtt_callback);            //atribui função de callback (função chamada quando qualquer informação de um dos tópicos subescritos chega)
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
+  MQTT.setCallback(mqtt_callback);            //atribui função de callback (função chamada quando qualquer informação de um dos tópicos subescritos chega)
 }
- 
-//Função: função de callback 
-//        esta função é chamada toda vez que uma informação de 
+
+//Função: função de callback
+//        esta função é chamada toda vez que uma informação de
 //        um dos tópicos subescritos chega)
 //Parâmetros: nenhum
 //Retorno: nenhum
-void mqtt_callback(char* topic, byte* payload, unsigned int length) 
+void mqtt_callback(char* topic, byte* payload, unsigned int length)
 {
-    String msg;
- 
-    //obtem a string do payload recebido
-    for(int i = 0; i < length; i++) 
-    {
-       char c = (char)payload[i];
-       msg += c;
-    }
-  
-    //toma ação dependendo da string recebida:
-    //verifica se deve colocar nivel alto de tensão na saída D4:
-    //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
-    //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
-    if (msg.equals("D"))
-    {
-        //digitalWrite(D4, LOW);
-        EstadoSaida = '1';
-    }
- 
-    //verifica se deve colocar nivel alto de tensão na saída D4:
-    if (msg.equals("L"))
-    {
-        //digitalWrite(D4, HIGH);
-        EstadoSaida = '0';
-    }
-    
+  String msg;
+
+  //obtem a string do payload recebido
+  for (int i = 0; i < length; i++)
+  {
+    char c = (char)payload[i];
+    msg += c;
+  }
+
+  //toma ação dependendo da string recebida:
+  //verifica se deve colocar nivel alto de tensão na saída D4:
+  //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
+  //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
+  if (msg.equals("D"))
+  {
+    //digitalWrite(D4, LOW);
+    EstadoSaida = '1';
+  }
+
+  //verifica se deve colocar nivel alto de tensão na saída D4:
+  if (msg.equals("L"))
+  {
+    //digitalWrite(D4, HIGH);
+    EstadoSaida = '0';
+  }
+
 }
- 
+
 //Função: reconecta-se ao broker MQTT (caso ainda não esteja conectado ou em caso de a conexão cair)
 //        em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
 //Parâmetros: nenhum
 //Retorno: nenhum
-void reconnectMQTT() 
+void reconnectMQTT()
 {
-    while (!MQTT.connected()) 
+  while (!MQTT.connected())
+  {
+    Serial.print("* Tentando se conectar ao Broker MQTT: ");
+    Serial.println(BROKER_MQTT);
+    if (MQTT.connect(ID_MQTT))
     {
-        Serial.print("* Tentando se conectar ao Broker MQTT: ");
-        Serial.println(BROKER_MQTT);
-        if (MQTT.connect(ID_MQTT)) 
-        {
-            Serial.println("Conectado com sucesso ao broker MQTT!");
-            MQTT.subscribe(TOPICO_SUBSCRIBE); 
-        } 
-        else 
-        {
-            Serial.println("Falha ao reconectar no broker.");
-            Serial.println("Havera nova tentatica de conexao em 2s");
-            delay(2000);
-        }
+      Serial.println("Conectado com sucesso ao broker MQTT!");
+      MQTT.subscribe(TOPICO_SUBSCRIBE);
     }
+    else
+    {
+      Serial.println("Falha ao reconectar no broker.");
+      Serial.println("Havera nova tentatica de conexao em 2s");
+      delay(2000);
+    }
+  }
 }
- 
+
 //Função: reconecta-se ao WiFi
 //Parâmetros: nenhum
 //Retorno: nenhum
-void reconectWiFi() 
+void reconectWiFi()
 {
-    //se já está conectado a rede WI-FI, nada é feito. 
-    //Caso contrário, são efetuadas tentativas de conexão
-    if (WiFi.status() == WL_CONNECTED)
-        return;
-        
-    WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
-    
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-        delay(100);
-        Serial.print(".");
-    }
-  
-    Serial.println();
-    Serial.print("Conectado com sucesso na rede ");
-    Serial.print(SSID);
-    Serial.println("IP obtido: ");
-    Serial.println(WiFi.localIP());
+  //se já está conectado a rede WI-FI, nada é feito.
+  //Caso contrário, são efetuadas tentativas de conexão
+  if (WiFi.status() == WL_CONNECTED)
+    return;
+
+  WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("Conectado com sucesso na rede ");
+  Serial.print(SSID);
+  Serial.println("IP obtido: ");
+  Serial.println(WiFi.localIP());
 }
- 
-//Função: verifica o estado das conexões WiFI e ao broker MQTT. 
+
+//Função: verifica o estado das conexões WiFI e ao broker MQTT.
 //        Em caso de desconexão (qualquer uma das duas), a conexão
 //        é refeita.
 //Parâmetros: nenhum
 //Retorno: nenhum
 void VerificaConexoesWiFIEMQTT(void)
 {
-    if (!MQTT.connected()) 
-        reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
-    
-     reconectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+  if (!MQTT.connected())
+    reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
+
+  reconectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
 }
- 
-//Função: envia ao Broker o estado atual do output 
+
+//Função: envia ao Broker o estado atual do output
 //Parâmetros: nenhum
 //Retorno: nenhum
 void EnviaEstadoOutputMQTT(void)
 {
-   distancia = ultrassom.Ranging(CM);// ultrassom.Ranging(CM) retorna a distancia em
-   //distancia = 1;                            // centímetros(CM) ou polegadas(INC) 
-   // if (EstadoSaida == '0')
-      MQTT.publish(TOPICO_PUBLISH, distancia);
- 
-   // if (EstadoSaida == '1')
-   //   MQTT.publish(TOPICO_PUBLISH, "D");
+  distancia = ultrassom.Ranging(CM); // ultrassom.Ranging(CM) retorna a distancia em
+  //char* dist = toChar(distancia, DEC);
+  //String att = "12334";
+  // centímetros(CM) ou polegadas(INC)
+  String dd = String(distancia);
+  char aa[23];
+  dd.toCharArray(aa,23);
+  char* tet = aa;
+  // if (EstadoSaida == '0')
+  MQTT.publish(TOPICO_PUBLISH, tet );
 
-   
-    Serial.print(distancia); //imprime o valor da variável distancia
-    Serial.print("cm"); 
-    Serial.println("- Estado da saida D4 enviado ao broker!");
-    delay(1000);
+  // if (EstadoSaida == '1')
+  //   MQTT.publish(TOPICO_PUBLISH, "D");
+
+
+  Serial.print(distancia); //imprime o valor da variável distancia
+  Serial.print("cm");
+  Serial.println(" - Estado da saida D4 enviado ao broker!");
+  delay(1000);
 }
- 
+
 //Função: inicializa o output em nível lógico baixo
 //Parâmetros: nenhum
 //Retorno: nenhum
 void InitOutput(void)
 {
-    //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
-    //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
-    //pinMode(D4, OUTPUT);
-    //digitalWrite(D4, HIGH);          
+  //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
+  //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
+  //pinMode(D4, OUTPUT);
+  //digitalWrite(D4, HIGH);
 }
 
 
 
 // Função que se repete infinitamente quando a placa é ligada
- void loop()
- {
-   //distancia = ultrassom.Ranging(CM);// ultrassom.Ranging(CM) retorna a distancia em
-   //distancia = 1;                            // centímetros(CM) ou polegadas(INC) 
-   //Serial.print(distancia); //imprime o valor da variável distancia
-   //Serial.println("cm");
-       
-   VerificaConexoesWiFIEMQTT(); //garante funcionamento das conexões WiFi e ao broker MQTT
- 
-   EnviaEstadoOutputMQTT();  //envia o status de todos os outputs para o Broker no protocolo esperado
-   
-   MQTT.loop(); //keep-alive da comunicação com broker MQTT
-   delay(300);
- }
+void loop()
+{
+  //distancia = ultrassom.Ranging(CM);// ultrassom.Ranging(CM) retorna a distancia em
+  //distancia = 1;                            // centímetros(CM) ou polegadas(INC)
+  //Serial.print(distancia); //imprime o valor da variável distancia
+  //Serial.println("cm");
 
- 
+  VerificaConexoesWiFIEMQTT(); //garante funcionamento das conexões WiFi e ao broker MQTT
+
+  EnviaEstadoOutputMQTT();  //envia o status de todos os outputs para o Broker no protocolo esperado
+
+  MQTT.loop(); //keep-alive da comunicação com broker MQTT
+  delay(300);
+}
+
+
